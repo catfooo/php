33232bde4dba2897db5e439b0ceacea3d1a4928b
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-11-18 13:12:05 +0000

    prevent refresh

--- Dag-05/variabler.php
+++ Dag-05/variabler.php
@@ -61,10 +61,12 @@ if(isset($firstName)){
 
         <hr>
         <h2>Ange pris</h2>
-        <?php
-// Debugging: Check if price is passed
-var_dump($_GET);
-?>
+        
+    <!-- Hidden inputs to keep previous data -->
+    <input type="hidden" name="firstName" value="<?php echo $firstName; ?>">
+            <input type="hidden" name="lastName" value="<?php echo $lastName; ?>">
+            <input type="hidden" name="DOB" value="<?php echo $DOB; ?>">
+
         <form action="" method="GET">
             <input type="number" name="price" id="">
             <input type="submit" value="Beräkna">
