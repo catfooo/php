b31c461ca936942be68e416e60ff5998be48a706
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-11-26 11:51:53 +0000

    .

--- projekt/index.php
+++ projekt/index.php
@@ -22,7 +22,7 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <title>Shop Homepage - Start Bootstrap Template</title>
+        <title>Maskrosaffären</title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Core theme CSS (includes Bootstrap)-->
@@ -73,7 +73,7 @@
         </section>
         <!-- Footer-->
         <footer class="py-5 bg-dark">
-            <div class="container"><p class="m-0 text-center text-white">Copyleft &copy; Your Website 2023</p></div>
+            <div class="container"><p class="m-0 text-center text-white">Maskrosaffären 2024</p></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
