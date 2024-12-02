326bb8608f53fd682e3e7ee9f601cb76a1d4bedf
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-02 13:25:33 +0000

    table name

--- php-2/confirm.php
+++ php-2/confirm.php
@@ -10,14 +10,14 @@
         $email = $_POST['email'];
         $message = $_POST['message'];
 
-        $sql = "INSERT INTO contacts(name, email, message)
+        $sql = "INSERT INTO tabell(name, email, message)
                 VALUES('$name', '$email', '$message')";
         
-        // if ($db->query($sql)) {
-        //     echo "<h1>Tack $name <br>Vi återkommer inom 24 timmar</h1>";
-        // } else {
-        //     echo $db->error;
-        // }
+        if ($db->query($sql)) {
+            echo "<h1>Tack $name <br>Vi återkommer inom 24 timmar</h1>";
+        } else {
+            echo $db->error;
+        }
         
 
     }
