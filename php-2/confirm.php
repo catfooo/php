ffe57622298e206026d03e72a36a193fb9e9d657
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-03 12:22:30 +0000

    it worked but might have last connection? idk

--- php-2/confirm.php
+++ php-2/confirm.php
@@ -4,7 +4,7 @@
         
         print_r($_POST);
 
-        $db = new mysqli("localhost", "root", "");
+        $db = new mysqli("localhost", "root", "", "contacts");
 
         $name = $_POST['name'];
         $email = $_POST['email'];
