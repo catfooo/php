6112c7680686170d8ca23b47a0f9740f345191c7
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-03 13:58:42 +0000

    _, ,

--- php-2/createtable.php
+++ php-2/createtable.php
@@ -3,11 +3,11 @@
     $db = new mysqli("localhost", "root", "", "myDB20241203135749");
     $name = "myTable" . date("YmdHis");
     $sql = "CREATE TABLE $name (
-        id INT(6) AUTO-INCREMENT PRIMARY KEY,
+        id INT(6) AUTO_INCREMENT PRIMARY KEY,
         firstname VARCHAR(50) NOT NULL,
         lastname VARCHAR(50) NOT NULL,
         email VARCHAR(50) NOT NULL,
-        reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
     )";
     $db->query($sql);
 
