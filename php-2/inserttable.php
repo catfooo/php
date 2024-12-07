aff17aaefa72ee4cae6eee4613101c5825716f25
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-07 10:28:00 +0000

    to see err msg

--- php-2/inserttable.php
+++ php-2/inserttable.php
@@ -1,13 +1,15 @@
 <?php
 
     $db = new mysqli("localhost", "root", "", "myDB20241203135749");
-    $sql = "INSERT INTO myTable20241203145907 (firstname, lastname, email) 
+    $sql = "INSERT INTO myTable2024120314590 (firstname, lastname, email) 
             VALUES ('rr', 'rr', 'rr@rr')";
     if ($db->query($sql)) {
         $id = $db->insert_id; // Get the ID of the last inserted row
         // Fetch the inserted row using the ID
         $result = $db->query("SELECT firstname, lastname, email FROM myTable20241203145907 WHERE id = $id");
         print_r($result->fetch_assoc());
+    } else {
+        echo "Error: " . $sql . "<br>" . $db->error;
     }
 
 ?>
