0d66e40e87c081c67ee3a86cdf44998ce1dc36df
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-08 15:06:37 +0000

    a

--- projekt/index.php
+++ projekt/index.php
@@ -73,7 +73,7 @@
         </section>
         <!-- Footer-->
         <footer class="py-5 bg-dark">
-            <div class="container"><p class="m-0 text-center text-white">Kontakt</p></div>
+            <div class="container"><a href="contact.php"><p class="m-0 text-center text-white">Kontakt</p></a></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
