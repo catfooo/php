769bac69f598381384215dd5e6ac4e33ab1e6ff7
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-08 17:43:09 +0000

    bootstrap

--- projekt/confirm.php
+++ projekt/confirm.php
@@ -1,27 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bekräftelse</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body class="container">
+    <h1 class="text-primary">bekräftelse</h1>
+    
+    
 <?php
 
 
 if (isset($_POST['submit'])) {
-        $db = new mysqli("localhost", "root", "root", "projekt");
-        // print_r($_POST);
-
-        $firstname = $_POST['firstname'];
-        $lastname = $_POST['lastname'];
-        $phone = $_POST['phone'];
-        $email = $_POST['email'];
-        $message = $_POST['message'];
-
-        echo "<h2>tack $firstname $lastname!</h2>";
-        echo "<p>ditt telefonnummer är $phone</p>";
-        echo "<p>vi kommer att ta kontakt med dig på följande e-post: $email</p>";
-        echo "<p>här nedan är ditt meddelande: <hr>$message<hr></p>";
-
-        $sql = "INSERT INTO kontakter(firstname, lastname, phone, email, message) 
+    $db = new mysqli("localhost", "root", "root", "projekt");
+    // print_r($_POST);
+    
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    
+    echo "<h2>tack $firstname $lastname!</h2>";
+    echo "<p>ditt telefonnummer är $phone</p>";
+    echo "<p>vi kommer att ta kontakt med dig på följande e-post: 
+         <span class='text-primary'>$email</span></p>";
+    echo "<p>här nedan är ditt meddelande: <hr>$message<hr></p>";
+    
+    $sql = "INSERT INTO kontakter(firstname, lastname, phone, email, message) 
                 VALUES ('$firstname', '$lastname', '$phone', '$email', '$message')";
 
-        $db->query($sql);
+$db->query($sql);
 
 
-    }
+}
 
 ?>
+
+
+</body>
+</html>
--- projekt/contact.php
+++ projekt/contact.php
@@ -4,21 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kontaktformulär</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Kontaktformulär</h1>
+    <h1 class="text-primary">Kontaktformulär</h1>
     <form action="confirm.php" method="post">
-        <label for="firstname">förnamn</label>
-        <input type="text" id="firstname" name="firstname" required placeholder="förnamn"><br>
-        <label for="lastname">efternamn</label>
-        <input type="text" id="lastname" name="lastname" required placeholder="efternamn"><br>
-        <label for="phone">telefonnummer</label>
-        <input type="text" id="phone" name="phone" required placeholder="telefonnummer"><br>
-        <label for="email">e-post</label>
-        <input type="email" id="email" name="email" required placeholder="e-post"><br>
-        <label for="message">meddelande</label>
-        <textarea name="message" id="message" required placeholder="skriv ett meddelande"></textarea><br>
-        <input type="submit" name="submit" value="skicka meddelandet">
+        <div class="row">
+            <div class="col-md-6">
+                <label for="firstname">förnamn</label>
+                <input type="text" id="firstname" name="firstname" required placeholder="förnamn" class="form-control">
+            </div>
+            <div class="col-md-6">
+                <label for="lastname">efternamn</label>
+                <input type="text" id="lastname" name="lastname" required placeholder="efternamn" class="form-control">
+            </div>
+            <div class="col-md-6">
+                <label for="phone">telefonnummer</label>
+                <input type="text" id="phone" name="phone" required placeholder="telefonnummer" class="form-control">
+            </div>
+            <div class="col-md-6">
+                <label for="email">e-post</label>
+                <input type="email" id="email" name="email" required placeholder="e-post"
+                class="form-control">
+            </div>
+            <div class="col-md-12">
+                <label for="message">meddelande</label>
+                <textarea name="message" id="message" required placeholder="skriv ett meddelande" class="form-control"></textarea>
+            </div>
+            <div class="col-md-4">
+                <input type="submit" name="submit" value="skicka meddelandet" class="btn btn-primary">
+            </div>
+        </div>
     </form>
 </body>
 </html>
