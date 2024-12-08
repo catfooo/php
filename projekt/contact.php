4dc31c0b7bad016a655f98f46c75c02a959645b7
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-08 17:44:54 +0000

    .

--- projekt/contact.php
+++ projekt/contact.php
@@ -6,7 +6,7 @@
     <title>Kontaktformulär</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body>
+<body class="container">
     <h1 class="text-primary">Kontaktformulär</h1>
     <form action="confirm.php" method="post">
         <div class="row">
