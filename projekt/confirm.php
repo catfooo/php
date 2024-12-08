6ee0449856b7a5fad58dd777cddf0b29917af3e9
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-08 16:45:16 +0000

    visa bekräftelse

--- projekt/confirm.php
+++ projekt/confirm.php
@@ -4,6 +4,19 @@
 if (isset($_POST['submit'])) {
         $db = new mysqli("localhost", "root", "root", "projekt");
         print_r($_POST);
+
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $phone = $_POST['phone'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+
+        echo "<h2>tack $firstname $lastname!</h2>";
+        echo "<p>ditt telefonnummer är $phone</p>";
+        echo "<p>vi kommer att ta kontakt med dig på följande e-post: $email</p>";
+        echo "<p>här nedan är ditt meddelande: <hr>$message</hr></p>";
+
+
     }
 
 ?>
