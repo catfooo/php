602e1b0340ec9c61669917c2fde1b71d9bc244f3
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-09 12:30:03 +0000

    fetch

--- php-3/index.php
+++ php-3/index.php
@@ -4,8 +4,22 @@
 
     if ($db->connect_error) {
         echo "failed to connect to MySQL";
-        // no further code executed
+        // no further code executed if err
         die();
+    }
+
+    // fetch
+    $sql = "SELECT * FROM films";
+
+    // kör
+    // -> : obj accesses query
+    $result = $db->query($sql);
+
+    // print
+    if ($result) {
+        // hämta alla rader som en array
+        $movies = $result->fetch_all(MYSQLI_ASSOC);
+        print_r($movies);
     }
 
 ?>
