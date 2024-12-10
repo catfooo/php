6907a3c86a0a116c5ae00c2c08acba5c2dc4e9d1
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-10 12:40:41 +0000

    beställningsformulär

--- php-3/order.php
+++ php-3/order.php
@@ -14,3 +14,14 @@
     print_r($film);
 
 ?>
+
+<h2>du har beställt <?php echo $film['title'];?></h2>
+<h3>artikelnummer: <?php echo $id;?></h3>
+<h4>pris: <?php echo $film['price']?>kr</h4>
+
+<form action="#" method="post">
+    <h3>ange din e-post</h3>
+    <input type="email" name="email" required> <br>
+    <input type="submit" name="submit" value="skicka beställningen">
+    <input type="hidden" name="film_id" value="<?php echo $id?>">
+</form>
