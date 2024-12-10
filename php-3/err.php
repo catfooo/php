914673718fe7887a985f3584e9a355f3bc451a3e
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-10 15:31:53 +0000

    .

--- php-3/err.php
+++ php-3/err.php
@@ -1,9 +1,9 @@
 <?php
-$con = mysqli_connect("localhost","my_user","my_password","my_db");
+$mysqli = new mysqli("localhost","my_user","my_password","my_db");
 
 // Check connection
-if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
   exit();
 }
 ?>
