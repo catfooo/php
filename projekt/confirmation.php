9f5d2993a17760d1311d14b72be9930ccfb6e252
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-10 14:46:25 +0000

    to show err msg when err happens

--- projekt/confirmation.php
+++ projekt/confirmation.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,22 +8,31 @@
     <!-- Core theme CSS (includes Bootstrap)-->
     <link href="css/styles.css" rel="stylesheet" />
 </head>
+
 <body class="container">
     <h1 class="text-primary text-center border-bottom border-primary">bekräftelse</h1>
 
     <?php
-        
-        if (isset($_POST['submit'])) {
-            // databasuppkoppling
-            require_once 'db.php';
 
-            echo "<h2>tack " . $_POST['name'] . "</h2>";
-            echo "<h3>ditt telefonnummer är " . $_POST['telephonenumber'] . "</h3>";
-            echo "<h3>ditt email är " . $_POST['email'] . "</h3>";
-            echo "<h3>vi kommer att leverera produkten till " . $_POST['address'] . "</h3>";
+    if (isset($_POST['submit'])) {
+        // databasuppkoppling
+        // require_once 'db.php';
+        $db = new mysqli("localhost", "ro00ot", "root", "projekt");
+
+        // quit if failed
+        if ($db->connect_error) {
+            echo "kunde inte koppla till databas";
+            die();
         }
 
+        echo "<h2>tack " . $_POST['name'] . "</h2>";
+        echo "<h3>ditt telefonnummer är " . $_POST['telephonenumber'] . "</h3>";
+        echo "<h3>ditt email är " . $_POST['email'] . "</h3>";
+        echo "<h3>vi kommer att leverera produkten till " . $_POST['address'] . "</h3>";
+    }
+
     ?>
-    
+
 </body>
+
 </html>
