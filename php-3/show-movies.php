460ecc17127e13860e441bc4a4a29830e165c543
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-10 15:41:07 +0000

    .

--- php-3/show-movies.php
+++ php-3/show-movies.php
@@ -1,6 +1,5 @@
 <?php
-    // error_reporting(0);  // Disable all error reporting
-    // ini_set('display_errors', 0);  // Disable displaying errors in the browser
+    
     
 
     require_once 'db.php';
