66bd07df99f80ba42306452c9825ea24ed1dd5a6
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-10 15:43:17 +0000

    .

--- projekt/db.php
+++ projekt/db.php
@@ -1,12 +1,14 @@
 <?php
+    
+    mysqli_report(MYSQLI_REPORT_OFF);
+    /* @ is used to suppress warnings */
+    $db = @new mysqli("localhost", "ro000ot", "root", "projekt");
 
-    $db = new mysqli("localhost", "root", "root", "projekt");
-
-    // // quit if failed
-    // if ($db->connect_error) {
-    //     echo "kunde inte koppla till databas";
-    //     die();
-    // }
+    // quit if failed
+    if ($db->connect_error) {
+        echo "kunde inte koppla till databas";
+        die();
+    }
 
 
 ?>
