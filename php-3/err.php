ff3a3ce507111b0ac23f47615c9595f5a3440d81
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-10 15:35:06 +0000

    https://www.php.net/manual/en/mysqli.connect-error.php

--- php-3/err.php
+++ php-3/err.php
@@ -1,9 +1,10 @@
 <?php
-$mysqli = new mysqli("localhost","my_user","my_password","my_db");
 
-// Check connection
-if ($mysqli -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  exit();
+mysqli_report(MYSQLI_REPORT_OFF);
+/* @ is used to suppress warnings */
+$mysqli = @new mysqli('localhost', 'fake_user', 'wrong_password', 'does_not_exist');
+if ($mysqli->connect_error) {
+    /* Use your preferred error logging method here */
+    echo "kys";
+    error_log('Connection error: ' . $mysqli->connect_error);
 }
-?>
