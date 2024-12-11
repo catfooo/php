73b9ffaf1beb140c2a9319c16ac9c046ff507777
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-11 12:59:24 +0000

    inköpslista

---
+++ uppgift-08-03/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>inköpslista</title>
+</head>
+<body>
+    <h1>inköpslista</h1>
+    <h2>vad vill du handla idag?</h2>
+    <form action="" method="post">
+        <input type="text" name="item">
+        <input type="submit" name="submit" value="lägg till">
+    </form>
+    <?php
+
+        if (isset($_POST['submit'])) {
+            $db = new mysqli("localhost", "root", "root", "items");
+            $item = $_POST['item'];
+            $sql = "INSERT INTO items (item) VALUES ('$item')";
+            $db->query($sql);
+            $result = $db->query("SELECT * FROM items");
+            if ($result) {
+                $items = $result->fetch_all(MYSQLI_ASSOC);
+                print_r($items);
+            }
+        }
+
+    ?>
+</body>
+</html>
