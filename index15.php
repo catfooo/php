beb8e00fda5aecc838faadf92c292c2837065e1e
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-14 14:50:33 +0000

    .

---
+++ index15.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <title>utskrift av datum</title>
+    <meta charset="utf-8">
+</head>
+<body>
+    <!-- skriver ut årtal, månad och dag enligt YYYY-MM-DD -->
+     <p>dagens datum är <?php echo date('Y-m-d'); ?></p>
+</body>
+</html>
