564be2eece5c024dc3b51c1ee90920948e362a00
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-14 14:40:45 +0000

    became bit bored...

---
+++ uppgift-08-04/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>telefonkatalog</title>
+</head>
+<body>
+    <h1>soyouns telefonlista</h1>
+    <form action="" method="post">
+        <input type="text" name="name" placeholder="ange namn" required>
+        <input type="text" name="phone" placeholder="ange telefon/mobil" required>
+        <input type="submit" value="lägg till">
+    </form>
+    
+</body>
+</html>
