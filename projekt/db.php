a4a9db943f964af42fd6abb00eaf2eb55d1a3677
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-15 23:21:50 +0000

    not worked

--- projekt/db.php
+++ projekt/db.php
@@ -3,7 +3,6 @@
     mysqli_report(MYSQLI_REPORT_OFF);
     /* @ is used to suppress warnings */
     $db = @new mysqli("localhost", "root", "root", "projekt");
-    $db->set_charset('utf8mb3');
 
     // quit if failed
     if ($db->connect_error) {
