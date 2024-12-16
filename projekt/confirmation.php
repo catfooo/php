16ca97345c0aa79629964f41fa022911782d8b61
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-16 19:58:03 +0000

    .

--- projekt/confirmation.php
+++ projekt/confirmation.php
@@ -4,13 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>bekr?ftelse</title>
+    <title>bekräftelse</title>
     <!-- Core theme CSS (includes Bootstrap)-->
     <link href="css/styles.css" rel="stylesheet" />
 </head>
 
 <body class="container">
-    <h1 class="text-primary text-center border-bottom border-primary">bekr?ftelse</h1>
+    <h1 class="text-primary text-center border-bottom border-primary">bekräftelse</h1>
 
     <?php
     
@@ -29,8 +29,8 @@
       
         
         echo "<h2>tack $name, du beställde produkt nr $articlenumber</h2>";
-        echo "<h3>ditt telefonnummer ?r $telephonenumber</h3>";
-        echo "<h3>ditt email ?r $email</h3>";
+        echo "<h3>ditt telefonnummer är $telephonenumber</h3>";
+        echo "<h3>ditt email är $email</h3>";
         echo "<h3>vi kommer att leverera produkten till $address</h3>";
 
         $sql = "INSERT INTO bestallningar (articlenumber, name, telephonenumber, email, address)
