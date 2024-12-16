a18700d0b2f2379acc2bfafd01f11421dd13d26f
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-16 00:56:15 +0000

    test iso

---
+++ projekt/test.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="ISO-8859-1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Maskrosaffären</title>
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico">
+</head>
+<body>
+    <header>
+        <h1>Maskrosaffären</h1>
+        <p>Vi säljer saker för maskrosor!</p>
+    </header>
+    
+    <section>
+        <h2>Våra produkter</h2>
+        <ul>
+            <li>Bröd</li>
+            <li>Vattenflaska</li>
+            <li>Täcke</li>
+            <li>Godis</li>
+            <li>Drömfångare</li>
+        </ul>
+    </section>
+    
+    <footer>
+        <p>Kontakt: <a href="contact.php">Kontaktinformation</a></p>
+    </footer>
+</body>
+</html>
