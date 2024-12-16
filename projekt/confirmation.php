39a7a8b5ab245477d8d85594f8b90762492e4bb0
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-16 01:52:19 +0000

    .

--- projekt/confirmation.php
+++ projekt/confirmation.php
@@ -4,13 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>bekr√§ftelse</title>
+    <title>bekr‰ftelse</title>
     <!-- Core theme CSS (includes Bootstrap)-->
     <link href="css/styles.css" rel="stylesheet" />
 </head>
 
 <body class="container">
-    <h1 class="text-primary text-center border-bottom border-primary">bekr√§ftelse</h1>
+    <h1 class="text-primary text-center border-bottom border-primary">bekr‰ftelse</h1>
 
     <?php
     
@@ -19,7 +19,7 @@
         // databasuppkoppling
         require_once 'db.php';
 
-        // h√§mta fr√•n form.php
+        // h‰mta frÂn form.php
         $name = $_POST['name'];
         $telephonenumber = $_POST['telephonenumber'];
         $email = $_POST['email'];
@@ -28,8 +28,8 @@
       
         
         echo "<h2>tack $name</h2>";
-        echo "<h3>ditt telefonnummer √§r $telephonenumber</h3>";
-        echo "<h3>ditt email √§r $email</h3>";
+        echo "<h3>ditt telefonnummer ‰r $telephonenumber</h3>";
+        echo "<h3>ditt email ‰r $email</h3>";
         echo "<h3>vi kommer att leverera produkten till $address</h3>";
 
         $sql = "INSERT INTO bestallningar (name, telephonenumber, email, address)
