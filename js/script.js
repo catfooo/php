64fd62d38b1b6e49eb37ff4ac9f9b531f89ff2ab
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-17 13:43:48 +0000

    .

--- js/script.js
+++ js/script.js
@@ -57,7 +57,7 @@ for (let index = 0; index < 10; index++) {
 alert(numbers);
 
 let lön = 1;
-for (let år = 0; lön < 10000000; index++) {
+for (let år = 0; lön < 10000000; år++) {
     lön += lön * 2 ** år
     
 }
