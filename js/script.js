65df99fa68b4d7aa621753e10001b9157849382f
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-18 10:59:54 +0000

    .

--- js/script.js
+++ js/script.js
@@ -60,7 +60,9 @@ let år = 0;
 let lön = 1;
 for (/*let år = 0, lön = 1*/; lön < 10000000; år++) {
     // lön += lön * 2 ** år
-    lön *= 2;
+    // lön *= 2; 
+    // lön += lön*= 2;
+    lön += lön * 2; // double and than add
     år = år ++
     
 }
