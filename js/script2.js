56416403da47b525b5dfff19591bfd586679e257
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-18 12:22:57 +0000

    .

--- js/script2.js
+++ js/script2.js
@@ -2,10 +2,6 @@
 
 const name = 'katt';
 const age = 8;
-const weight = 4.5;
-const isGullig = true
-const x = null;
-const y = undefined;
-let z;
 
-console.log(typeof z);
+// concatenation
+console.log('her name is ' + katt + 'and age is ' + age + '.')
