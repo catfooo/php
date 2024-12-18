65e02637814a55f6ffd67ba750965a0019ae6e3e
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-18 14:52:32 +0000

    .

--- js/script2.js
+++ js/script2.js
@@ -22,3 +22,10 @@ const memories = [
 for(let i = 0; i <= 10; i++) {
     console.log(`for loop number: ${i}`);
 }
+
+// while
+let i = 0;
+while(i < 10){
+    console.log(`while loop number: ${i}`);
+    i++;
+}
