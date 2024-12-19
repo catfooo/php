acaa6187e1d2e8a0fb0aa0382145863a6888b46e
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-19 10:27:01 +0000

    .

--- js/script2.js
+++ js/script2.js
@@ -1,7 +1,9 @@
 const x = 20;
 
 if(x === 10) {
-    console.log('x is 10')
+    console.log('x is 10');
+} else if(x > 10) {
+    console.log('x is greater than 10');
 } else {
-    console.log('x is not 10')
+    console.log('x is not 10');
 }
