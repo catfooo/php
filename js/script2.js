9409a8837dcf3eb4c4d6e9f13a7dced9c5608465
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-19 10:40:11 +0000

    .

--- js/script2.js
+++ js/script2.js
@@ -1,6 +1,7 @@
-const x = 6;
-const y = 11;
+// ternary operator
 
-if(x > 5 && y > 10) {
-    console.log('x is more than 5 and y is more than 10');
-} 
+const x = 10;
+
+const color = x > 10 ? 'red' : 'blue';
+
+console.log(color);
