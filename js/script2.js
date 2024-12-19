0b2b007e35d18b6451ec752229e4066dae0cbdf1
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-19 18:13:03 +0000

    this is safari

--- js/script2.js
+++ js/script2.js
@@ -8,7 +8,7 @@ function Person(firstName, lastName, dob) {
 }
 
 // instantiate object
-const person1 = new Person('kitty', 'katty', '1-2-2005');
-const person2 = new Person('kotty', 'kutty', '1-8-2006');
+const person1 = new Person('kitty', 'katty', '2005-2-1');
+const person2 = new Person('kotty', 'kutty', '2006-8-1');
 
 console.log(person2.dob);
