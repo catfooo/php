8acce3c8a95824e6a9059c304ae38c838dd41187
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-19 18:23:00 +0000

    .

--- js/script2.js
+++ js/script2.js
@@ -17,5 +17,5 @@ function Person(firstName, lastName, dob) {
 const person1 = new Person('kitty', 'katty', '2005-02-01');
 const person2 = new Person('kotty', 'kutty', '2006-08-01');
 
-console.log(person1.getBirthYear());
-console.log(person1.getFullName());
+
+console.log(person1);
