621c6de6c3833a36aa3c81429d4c6cfa4fb9b950
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-19 18:08:34 +0000

    .

--- js/script2.js
+++ js/script2.js
@@ -9,5 +9,6 @@ function Person(firstName, lastName, dob) {
 
 // instantiate object
 const person1 = new Person('kitty', 'katty', '1-2-2005');
+const person2 = new Person('kotty', 'kutty', '1-8-2006');
 
-console.log(person1);
+console.log(person2.firstName);
