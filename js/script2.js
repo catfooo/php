985ac265f63b25b72fdbf6c8879eea57e5ae24ea
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-19 10:49:53 +0000

    .

--- js/script2.js
+++ js/script2.js
@@ -1,7 +1,17 @@
-// ternary operator
+// switch
 
 const x = 11;
 
 const color = x > 10 ? 'red' : 'blue';
 
-console.log(color);
+switch(color) {
+    case 'red':
+        console.log('color is red');
+        break;
+    case 'blue':
+        console.log('color is blue');
+        break;
+    default:
+        console.log('color is NOT red or blue');
+        break;
+}
