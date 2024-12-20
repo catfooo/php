8a9d3c1f4238d2fb0e3833a95aaba6bcb87dd010
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-20 13:32:17 +0000

    .

--- js/Project/main.js
+++ js/Project/main.js
@@ -2,7 +2,7 @@
 
 const btn = document.querySelector('.btn');
 
-btn.addEventListener('mouseover', (e) => {
+btn.addEventListener('mouseout', (e) => {
   e.preventDefault();
   document.querySelector('#my-form').style.background = '#ccc';
   document.querySelector('body').classList.add('bg-dark');
