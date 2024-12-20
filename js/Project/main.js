27b3f7ea95797239fd426bfbecd825bcf856e389
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-20 13:01:19 +0000

    .

--- js/Project/main.js
+++ js/Project/main.js
@@ -7,4 +7,4 @@ ul.children[1].innerText = 'katty';
 ul.lastElementChild.innerHTML = '<h1>hej</h1>';
 
 const btn = document.querySelector('.btn');
-btn.getElementsByClassName.backgrund = 'red';
+btn.getElementsByClassName.background = 'red';
