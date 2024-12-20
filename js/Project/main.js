a6f2fde4672a4133670a0a12b84a54e56442f445
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-20 13:28:13 +0000

    .

--- js/Project/main.js
+++ js/Project/main.js
@@ -5,4 +5,5 @@ const btn = document.querySelector('.btn');
 btn.addEventListener('click', (e) => {
   e.preventDefault();
   document.querySelector('#my-form').style.background = '#ccc';
+  document.querySelector('body').classList.add('bg-dark');
 });
