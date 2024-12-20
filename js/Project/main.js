7d3246ffff4c56660596c5874058017d28460323
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-20 12:30:01 +0000

    .

--- js/Project/main.js
+++ js/Project/main.js
@@ -1,7 +1,8 @@
-// single element 
-console.log(document.getElementById('my-form'));
-console.log(document.querySelector('h1'));
+// // single element 
+// console.log(document.getElementById('my-form'));
+// console.log(document.querySelector('h1'));
 
 
 // multiple element
 console.log(document.querySelectorAll('.item'));
+console.log(document.getElementsByClassName('item'));
