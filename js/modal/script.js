6136152cce628754f639eaa8b79011a579da30ad
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-22 11:01:52 +0000

    https://www.w3schools.com/howto/howto_css_modal_images.asp

---
+++ js/modal/script.js
@@ -0,0 +1,19 @@
+// get the modal
+let modal = document.getElementById('modal');
+
+// get img and insert into modal, use alt as caption
+let img = document.getElementById('img');
+let imgContent = document.getElementById('img-content');
+let captionText = document.getElementById('caption');
+
+img.onClick = function() {
+    modal.style.display = "block";
+    imgContent.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// span closes the modal
+let span = document.getElementsByClassName('close')[0];
+span.onClick = function() {
+    modal.style.display = "none";
+}
