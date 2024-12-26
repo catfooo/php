8647109fc2e191fcf1913f9f384016b2980609d7
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-26 14:05:18 +0000

    .

--- projekt/landing.php
+++ projekt/landing.php
@@ -9,7 +9,7 @@
 
     $client->setClientId($_ENV["OAUTH_CLIENTID"]);
     $client->setClientSecret($_ENV["OAUTH_CLIENTSECRET"]);
-    $client->setRedirectUri("http://localhost:8888/projekt/redirect.php");
+    $client->setRedirectUri("http://212.18.224.194/~okt2404/projekt/redirect.php");
 
     $client->addScope("email");
     $client->addScope("profile");
