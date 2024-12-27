5a62069a594e154b58dbed053999e4c982eaddd9
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-27 13:27:14 +0000

    when confirm order, save user id from session, to further track item list by user id

--- projekt/confirmation.php
+++ projekt/confirmation.php
@@ -1,11 +1,13 @@
 <?php
 
+    session_start();
+    
     require_once "header.php";
 
 ?>
 
 <section class="container">
-    <h1 class="text-primary text-center border-bottom border-primary">bekräftelse</h1>
+    <h1 class="text-primary text-center border-bottom border-primary">bekr?ftelse</h1>
 
     <?php
     
@@ -21,15 +23,19 @@
         $email = $_POST['email'];
         $address = $_POST['address'];
 
+        // hämta från sessionen
+        $user_id = $_SESSION['user_id'];
+        
+
       
         
-        echo "<h2>tack $name, du beställde produkt nr $articlenumber</h2>";
-        echo "<h3>ditt telefonnummer är $telephonenumber</h3>";
-        echo "<h3>ditt email är $email</h3>";
+        echo "<h2>tack $name, du best?llde produkt nr $articlenumber</h2>";
+        echo "<h3>ditt telefonnummer ?r $telephonenumber</h3>";
+        echo "<h3>ditt email ?r $email</h3>";
         echo "<h3>vi kommer att leverera produkten till $address</h3>";
 
-        $sql = "INSERT INTO bestallningar (articlenumber, name, telephonenumber, email, address)
-                VALUES ('$articlenumber', '$name', '$telephonenumber', '$email', '$address')";
+        $sql = "INSERT INTO bestallningar (articlenumber, user_id, name, telephonenumber, email, address)
+                VALUES ('$articlenumber', '$user_id', '$name', '$telephonenumber', '$email', '$address')";
 
         $db->query($sql);
 
