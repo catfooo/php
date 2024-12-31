8a6fd8e4e2b7d5c5421f053d9dc26577f26b9c56
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-31 12:00:32 +0000

    .

--- projekt/redirect.php
+++ projekt/redirect.php
@@ -71,7 +71,7 @@
                 window.onload = function() {
                     setTimeout(() => {
                         location.href = 'http://localhost:8888/projekt/grid.php';
-                    }, 1000);
+                    }, 500);
                 }
           </script>";
 
