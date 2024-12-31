7a76ee1573c3a2e896e286f3f4916f06823d9868
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2024-12-31 15:20:43 +0000

    .

--- projekt/grid.php
+++ projekt/grid.php
@@ -48,6 +48,8 @@
             // Check if the tile is 3
             if ($i == 3) {
                 echo "<div class='tile shop'><a href='http://localhost:8888/projekt/index.php'>&#8962;</a></div>"; // Display the house symbol ?
+            } elseif($i == 2) {
+                echo "☺☻";
             } elseif ($i == 1) {
                 echo "<div class=''><</div>";
             } elseif ($i == 13) {
