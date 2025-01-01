77a9c5223722b6fd62d132229a6c9f604ea2f3bc
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2025-01-01 12:12:53 +0000

    reproduce ☺☻ display

--- fds.php
+++ fds.php
@@ -4,9 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>for divs sake</title>
+    <link rel="stylesheet" href="projekt/css/style.css">
 </head>
 <body>
     <div>☺☻</div>
+    <?php
+        
+        echo '<div class="tileset">';
+        for ($i=1; $i <=25; $i++) { 
+            echo "<div class='tile'>$i ☺☻</div>";
+        }
+        echo '</div>';
+
+    ?>
     
 </body>
 </html>
