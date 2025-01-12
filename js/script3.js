06ba21efadc203cbca432af22f179398b75950cf
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2025-01-12 08:19:16 +0000

    .

--- js/script3.js
+++ js/script3.js
@@ -4,9 +4,11 @@ for (lön = 1, sum = 0; sum < 10000000; dag++) {
     sum += lön
     lön *= 2
     console.log(dag, sum, lön)
+
+    if(sum >= 10000000) {alert(dag)}
     
 }
-console.log(`det tar ${dag} dagar att tjena ihop 10 miljoner kronor`)
+//console.log(`det tar ${dag} dagar att tjena ihop 10 miljoner kronor`)
 //alert(dag)
 
-document.getElementById("result").innerHTML = `det tar ${dag} dagar att tjena ihop 10 miljoner kronor`
+//document.getElementById("result").innerHTML = `det tar ${dag} dagar att tjena ihop 10 miljoner kronor`
