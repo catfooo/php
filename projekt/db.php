362775977b89936b455c4638efc447452f11a5a8
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2025-01-12 14:08:31 +0000

    latin tweak became not needed(server changed?)

--- projekt/db.php
+++ projekt/db.php
@@ -3,7 +3,8 @@
     mysqli_report(MYSQLI_REPORT_OFF);
     /* @ is used to suppress warnings */
     $db = @new mysqli("localhost", "root", "root", "projekt");
-    $db->set_charset('latin1'); // This sets the database charset to ISO-8859-1
+    // latin tweak became not needed(server changed?)
+    //$db->set_charset('latin1'); // This sets the database charset to ISO-8859-1
 
     // quit if failed
     if ($db->connect_error) {
