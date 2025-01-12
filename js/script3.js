0c75638809a77fda316343dcd7b83c18a0b28c1a
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2025-01-12 08:20:28 +0000

    .

--- js/script3.js
+++ js/script3.js
@@ -3,7 +3,7 @@ let dag = 0
 for (lön = 1, sum = 0; sum < 10000000; dag++) {
     sum += lön
     lön *= 2
-    console.log(dag, sum, lön)
+    //console.log(dag, sum, lön)
 
     if(sum >= 10000000) {alert(dag + 1)}
     
