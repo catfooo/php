27170e1dd259216fe86d7d95be81916a8f93a9fe
So Youn Choi <70469440+catfooo@users.noreply.github.com>
2025-01-12 07:43:23 +0000

    .

--- js/script3.js
+++ js/script3.js
@@ -1,4 +1,4 @@
-for (let dag = 0, lön = 1, sum = 0; sum < 10000000; dag++) {
+for (let dag = 1, lön = 1, sum = 0; sum < 10000000; dag++) {
     sum += lön
     lön *= 2
     console.log(dag, sum, lön)
